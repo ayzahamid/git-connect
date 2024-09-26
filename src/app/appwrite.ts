67dd8cd59bc4393932e9ed38a4887f435e2a03c2
Ayza Hamid <ayzahamid222@gmail.com>
2024-09-26 13:20:03 +0500

    Setup signup/signin form

--- src/app/appwrite.ts
+++ src/app/appwrite.ts
@@ -1,11 +1,11 @@
-import { Client, Account, ID } from 'appwrite';
+import { Client, Account, ID } from 'appwrite'
 
-const client = new Client();
+const client = new Client()
 
 client
-    .setEndpoint(process.env.APP_WRITE_ENDPOINT || '') // Your API Endpoint
-    .setProject(process.env.APP_WRITE_PROJECT_ID || ''); // Your project ID
+    .setEndpoint(process.env.NEXT_PUBLIC_APP_WRITE_PROJECT_ID || '')
+    .setProject(process.env.NEXT_PUBLIC_APP_WRITE_ENDPOINT || '')
 
-const account = new Account(client);
+const account = new Account(client)
 
-export { client, account, ID };
+export { client, account, ID }
